68250a519d8b2edd708d39b175fa30ab34de4ea3
Byeon-Jaewon <bjw5095@gmail.com>
2020-05-16 23:39:33 +0900

    board_list and write

---
+++ 1.sql
@@ -0,0 +1,7 @@
+#조회, 등록, 수정, 삭제 CRUD CREATE Retrieve UPDATE DELETE
+
+select * from test.board
+
+insert into test.board (b_title, b_note, b_writer)
+values('title2', 'note2', 'ㅋ')
+
--- test/board/urls.py
+++ test/board/urls.py
@@ -5,4 +5,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name="home"),
+    path('board', views.board, name="board"),
+    path('board_write', views.board_write, name="board_write"),
+    path('board_insert', views.board_insert, name="board_insert"),
 ]
+ 
